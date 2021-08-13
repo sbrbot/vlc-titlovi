21788a9da45d1ab44a3223975f156b5d79fe0453
Stjepan Brbot <stjepan.brbot@gmail.com>
2021-08-13 21:53:35 +0200

    v1.2
    added filter for season and episode

--- extensions/vlc-titlovi.lua
+++ extensions/vlc-titlovi.lua
@@ -15,10 +15,20 @@ biblioteke: zzlib, inflate-bit32 i numberlua
 
 
 title = "VLC-Titlovi"
-version = "1.1"
+version = "1.2"
 program = title .. " " ..version
 
-icon = "\137\80\78\71\13\10\26\10\0\0\0\13\73\72\68\82\0\0\0\32\0\0\0\32\4\3\0\0\0\129\84\103\199\0\0\0\27\80\76\84\69\130\48\69\29\29\29\69\69\69\94\94\94\121\121\121\146\146\146\177\177\177\201\201\201\234\234\234\126\204\192\130\0\0\0\1\116\82\78\83\0\64\230\216\102\0\0\0\124\73\68\65\84\40\207\99\96\160\46\96\20\20\64\225\43\10\10\10\34\113\211\59\138\145\5\4\5\35\58\58\2\225\2\108\229\138\130\194\29\29\197\48\179\68\59\58\90\4\5\213\18\224\102\11\122\0\149\11\32\89\38\40\92\158\128\98\59\138\109\80\21\229\37\130\130\225\229\229\138\2\16\1\176\149\106\64\107\4\33\2\98\29\29\205\130\194\21\29\109\130\96\1\38\37\36\160\64\164\64\40\28\224\18\160\135\22\6\100\45\212\137\79\0\21\236\54\197\104\55\24\83\0\0\0\0\73\69\78\68\174\66\96\130"
+icon = "\137\80\78\71\13\10\26\10\0\0\0\13\73\72\68\82\0\0\0\32\0\0\0\32\4\3\0\0"..
+       "\0\129\84\103\199\0\0\0\27\80\76\84\69\130\48\69\29\29\29\69\69\69\94\94"..
+       "\94\121\121\121\146\146\146\177\177\177\201\201\201\234\234\234\126\204"..
+       "\192\130\0\0\0\1\116\82\78\83\0\64\230\216\102\0\0\0\124\73\68\65\84\40"..
+       "\207\99\96\160\46\96\20\20\64\225\43\10\10\10\34\113\211\59\138\145\5\4"..
+       "\5\35\58\58\2\225\2\108\229\138\130\194\29\29\197\48\179\68\59\58\90\4\5"..
+       "\213\18\224\102\11\122\0\149\11\32\89\38\40\92\158\128\98\59\138\109\80"..
+       "\21\229\37\130\130\225\229\229\138\2\16\1\176\149\106\64\107\4\33\2\98"..
+       "\29\29\205\130\194\21\29\109\130\96\1\38\37\36\160\64\164\64\40\28\224"..
+       "\18\160\135\22\6\100\45\212\137\79\0\21\236\54\197\104\55\24\83\0\0\0\0"..
+       "\73\69\78\68\174\66\96\130"
 
 dlg_cbox = {}
 dlg_sel_set = false
@@ -81,15 +91,15 @@ function activate()
 
   --
 
-  dlg:add_label("<small>Sortiranje rezultata:</small>",1,3,40,1)
-  dlg:add_label("<small>Tip sadržaja:</small>",41,3,40,1)
+  dlg:add_label("<small>Sortiranje rezultata:</small>",1,3,60,1)
+  dlg:add_label("<small>Tip sadržaja:</small>",61,3,20,1)
   dlg:add_label("<small>Uploader:</small>",81,3,20,1)
   
-  dlg_sort = dlg:add_dropdown(1,4,40,2)
+  dlg_sort = dlg:add_dropdown(1,4,60,2)
   for key,val in pairs(sortiranje) do
     dlg_sort:add_value(val,key)
   end
-  dlg_type = dlg:add_dropdown(41,4,40,2)
+  dlg_type = dlg:add_dropdown(61,4,20,2)
   for key,val in pairs(tipovi) do
     dlg_type:add_value(val,key)
   end
@@ -98,9 +108,13 @@ function activate()
   --
 
   dlg:add_label(" ",1,7,80)
-  dlg:add_label("<small>Dio imena za pretraživanje:</small>",1,8,80)
-
-  dlg_txt = dlg:add_text_input(getKeywords(getVideoNameWoExt()),1,9,80,1)
+  dlg:add_label("<small>Dio imena za pretraživanje:</small>",1,8,60)
+  dlg:add_label("<small>Sezona:</small>",61,8,10)
+  dlg:add_label("<small>Epizoda:</small>",71,8,10)
+  
+  dlg_txt = dlg:add_text_input(cleanKeywords(getVideoNameWoExt()),1,9,60)
+  dlg_sez = dlg:add_text_input(string.match(getVideoNameWoExt(),".*[sS](%d?%d)"),61,9,10)
+  dlg_epi = dlg:add_text_input(string.match(getVideoNameWoExt(),".*[eE](%d?%d)"),71,9,10)
   dlg_btn = dlg:add_button("Pronađi",find_first,81,9,20,1)
 
   dlg_res = dlg:add_label(getFilePath(),1,10,80)
@@ -113,9 +127,9 @@ function activate()
   dlg_lst = dlg:add_list(1,12,80,32)
   dlg:add_label("<center><small>stranica</small></center>",81,12,20)
   
-  dlg:add_button("Učitaj",load,81,13,20)
+  dlg:add_button("Učitaj",load,81,15,20)
 
-  dlg_typ = dlg:add_label("",81,14,20) --ZIP/SRT
+  dlg_typ = dlg:add_label("",81,16,20) --ZIP/SRT
 
   if(getFilePath() ~= "") then
     dlg_map = dlg:add_label("<center><a href=\"file:///" .. getFilePath() .. "\">Folder</a></center>",81,30,20)
@@ -154,6 +168,8 @@ end
 function find(pg)
 
   r = 0
+  
+  local s,e
 
   if(dlg_txt:get_text() ~= "") then
 
@@ -164,18 +180,42 @@ function find(pg)
     dlg_icn = dlg:add_spin_icon(89,7,4,2)
     dlg_icn:animate()
     dlg:update()
-
-    local url = "https://titlovi.com/titlovi/?prijevod=" .. vlc.strings.encode_uri_component(dlg_txt:get_text()) .. "&pg=" .. pg
+    
+    local url = "https://titlovi.com/titlovi/?prijevod=" .. vlc.strings.encode_uri_component(dlg_txt:get_text())
 
     local ojezici = {}
     for key,val in pairs(jezici) do
       if dlg_cbox[key]:get_checked() then table.insert(ojezici,val[2]) end
     end
 
-    if(table.concat(ojezici,"|") ~= "") then url = url .. "&jezik=" .. table.concat(ojezici,"|") end
-    if(dlg_sort:get_value() ~= 0) then url = url .. "&sort=" .. dlg_sort:get_value() end
-    if(dlg_type:get_value() ~= 0) then url = url .. "&t=" .. dlg_type:get_value() end
-    if(dlg_uld:get_text() ~= "") then url = url .. "&korisnik=" .. string.gsub(dlg_uld:get_text()," ","%+") end
+    if(table.concat(ojezici,"|") ~= "") then 
+      url = url .. "&jezik=" .. table.concat(ojezici,"|") 
+    end
+    if(dlg_sort:get_value() ~= 0) then 
+      url = url .. "&sort=" .. dlg_sort:get_value() 
+    end
+    if(dlg_type:get_value() ~= 0) then 
+      url = url .. "&t=" .. dlg_type:get_value() 
+    end
+    if(dlg_sez:get_text() ~= "") then
+      s = tonumber(dlg_sez:get_text())
+      if not s then s = 0 end
+      dlg_sez:set_text(s)
+      url = url .. "&s=" .. s 
+    end
+    if(dlg_epi:get_text() ~= "") then 
+      e = tonumber(dlg_epi:get_text())
+      if(not e or s == 0) then e = 0 end
+      dlg_epi:set_text(e)
+      url = url .. "&e=" .. e
+    end
+    if(dlg_uld:get_text() ~= "") then 
+      url = url .. "&korisnik=" .. vlc.strings.encode_uri_component(dlg_uld:get_text()) 
+    end
+    
+    url = url .. "&pg=" .. pg
+    
+    vlc.msg.info("[VLC-Titlovi] url = " .. url)
     
     local data = vlc.stream(url):read(4^8);
 
@@ -187,7 +227,7 @@ function find(pg)
 
       ul = string.sub(data,string.find(data,"<ul class=\"titlovi.+<div class=\"paging\">"))
 
-      local s,e = string.find(ul,"<h3.-h5>")
+      s,e = string.find(ul,"<h3.-h5>")
       while (e ~= nil) do
       
         li = string.sub(ul,s,e)
@@ -206,6 +246,11 @@ function find(pg)
 
         ss,ee = string.find(li,"alt=\"%d\"")
         lang = tonumber(string.sub(li,ss+5,ee-1))
+
+        --[[
+        naslov = naslov .. " " string.match(li,"<h4>(.-)<span")
+        lang = tonumber(string.match(li,"alt=\"(%d)\""))        
+        --]]
         
         dlg_lst:add_value("[" .. jezici[lang][1] .. "] " .. naslov,id)
 
@@ -245,6 +290,7 @@ function load()
     url = url .. key
 
     local stream = vlc.stream(url)
+    if not stream then vlc.msg.err("[VLC-Titlovi] Sajt Titlovi.com nedostupan ") return false end
     local data = stream:read(8^4)
     while(data ~= nil and data ~= "") do
       buf = buf .. data
@@ -263,7 +309,7 @@ function load()
         for key,val in pairs(filenames) do
           dlg_lst:add_value(val,key)
         end
-        dlg_sel = dlg:add_button("Odaberi",select,81,13,20)
+        dlg_sel = dlg:add_button("Odaberi",select,81,15,20)
         dlg_sel_set = true
       end
     else
@@ -307,13 +353,6 @@ function save(data,filename)
 
 end
 
-function getVideoNameWoExt()
-  if(getVideoName() ~= "") then
-    return getVideoName():match("(.+)%..+$")
-  end
-  return ""
-end
-
 --
 
 function about()
@@ -338,7 +377,14 @@ end
 
 --
 
-function getVideoName()   
+function getVideoNameWoExt()
+  if(getVideoName() ~= "") then
+    return getVideoName():match("(.+)%.")
+  end
+  return ""
+end
+
+function getVideoName()
   return (vlc.input.item()) and vlc.input.item():name() or  ""
 end
 
@@ -361,17 +407,21 @@ end
 
 function getTagContent(html,tag)
   local s1,e1 = string.find(html,"<" .. tag .. ".-" .. ">")
+  if(e1 == nil) then return "" end
   local s2,e2 = string.find(html,"</" .. tag .. ">",e1)
-  return (e1 ~= nil and s2 ~= nil) and string.sub(html,e1+1,s2-1) or ""
+  if(s2 == nil) then return "" end
+  return string.sub(html,e1+1,s2-1)
+  --return string.match(html,"<"..tag..".->(.-)<%s*%/%s*"..tag.."%s*>")
 end
 
 function getTagAttribute(html,attr)
   local s1,e1 = string.find(html,attr .. "=\"")
   local s2,e2 = string.find(html,"\"",e1+1)
   return (e1 ~= nil and s2 ~= nil) and string.sub(html,e1+1,s2-1) or ""
+  --return string.match(html,attr.."%s*=%s*\"(%d+)\"")
 end
 
-function getKeywords(text)
+function cleanKeywords(text)
   if(text ~= "") then
     --oznake koje se uklanjaju iz naziva datoteke filma za bolje pretraživanje titla
     local pattern = { "360p","480p","720p","1080p","2160p","4320p","HEVC","XviD","XVID",
@@ -386,6 +436,8 @@ function getKeywords(text)
     text = string.gsub(text,"%."," ")
     text = string.gsub(text,"%[","")
     text = string.gsub(text,"%]"," ")
+    text = string.gsub(text,"[sS]%d?%d","")
+    text = string.gsub(text,"[eE]%d?%d","")
     for key,val in pairs(pattern) do
       text = string.gsub(text," " .. val,"")
     end
